6b65f4b34f8afd5b6c0704806ceb542286d5a17b
stephen.wickwire <stephen.wickwire@MU-C1-12.mullen.pvt.k12.co.us>
2016-08-25 09:29:15 -0600

    initial commit

---
+++ ModulusPractice/src/Challenges.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Challenges
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Type any number you want");
+				Scanner sc = new Scanner(System.in);
+				int number = sc.nextInt();
+			}
+
+	}
